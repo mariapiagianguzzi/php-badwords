e7657254dd82345ccd27e0cc267b2ff013aaf049
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-01-28 17:24:05 +0100

    php-badwords

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- Passare due argomenti allo script: Nome e Cognome.
+Lo script dovrà salutare l’interlocutore. -->
+
+<?php 
+/* $nome = $argv[1];
+$cognome = $argv[2]; */
+$text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore facilis in distinctio, ducimus esse accusamus reprehenderit illum molestiae aut ex quaerat deleniti voluptatem aliquid earum perspiciatis ipsa animi quibusdam!";
+$saluto = $_GET ["badwords"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1>Ciao <?php echo $nome . " " . $cognome;?> </h1> -->
+    <p> </p>
+    <script src="main.js" charset="utf-8"></script>
+</body>
+</html>
